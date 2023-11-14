94ceb2c0f8dfede0f280a8414c3393f09af7d0a6
pawel <plapacz6@gmail.com>
2023-11-14 16:41:02 +0100

    Tpomodoro_cfg : write_cfg(), read_cfg()

--- lib/include/pomodoro_en/Tpomodoro_cfg.h
+++ lib/include/pomodoro_en/Tpomodoro_cfg.h
@@ -1,5 +1,6 @@
 #ifndef TPOMODORO_CFG_H
 #define TPOMODORO_CFG_H
+#include <string>
 
 namespace namespace_plapacz6 {
 
@@ -11,6 +12,9 @@ class Tpomodoro_cfg {
         int long_break_time;
 
         void set_default_parameters();
+        void write_cfg(const std::string& cfg_fname_);
+        void read_cfg(const std::string& cfg_fname_);
+
         Tpomodoro_cfg();
         ~Tpomodoro_cfg();
     private:
